b272a4aa7d0c97c798437721bc9047d0b6522feb
dmitry <whtperson@gmail.com>
2023-05-01 09:31:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+// Цена билета
+        int costTicket = 20000;
+// каждые 20 рублей равняются одной мили
+        int rublesPerMiles = 20;
+// расчет количества бонусных милей
+        int bonus = (costTicket / rublesPerMiles);
+        System.out.println( "Бонусные мили: " + bonus);
+
+
+    }
+}
